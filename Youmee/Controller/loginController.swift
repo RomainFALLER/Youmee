639a1d06c2324059ef2eaccc3dae75b5076790a4
FALLER Romain <romain.faller@gmail.com>
2019-02-17 16:51:50 +0100

    Login view version 1 with constraints.

--- Youmee/Controller/loginController.swift
+++ Youmee/Controller/loginController.swift
@@ -1,19 +1,19 @@
 //
-//  ViewController.swift
+//  loginController.swift
 //  Youmee
 //
-//  Created by FALLER Romain on 16/02/2019.
+//  Created by FALLER Romain on 17/02/2019.
 //  Copyright © 2019 FALLER Romain. All rights reserved.
 //
 
 import UIKit
-class ViewController: UIViewController {
 
+class loginController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
 }
-
---
+++ Youmee/loginModel.swift
@@ -0,0 +1,9 @@
+//
+//  loginModel.swift
+//  Youmee
+//
+//  Created by FALLER Romain on 17/02/2019.
+//  Copyright © 2019 FALLER Romain. All rights reserved.
+//
+
+import Foundation

6227bb85b4a5665102433dee109541f265419217
Fagot <fagotbenoit83@gmail.com>
2019-02-18 00:04:30 +0100

    update first cell from profile tableview

---
+++ Youmee/Controller/Profile/ProfileVC.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileVC.swift
+//  Youmee
+//
+//  Created by Benoit Fagot on 17/02/2019.
+//  Copyright © 2019 FALLER Romain. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    @IBOutlet weak var profilTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profilTableView.rowHeight = UITableView.automaticDimension
+        profilTableView.estimatedRowHeight = 600
+        profilTableView.reloadData()
+    }
+}
+extension ProfileVC :UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        if row == 0{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileInformationCellIdentifier") as! ProfileInformationCell
+            return cell
+        }
+        else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileNetworkCellIdentifier") as! NetworkTableViewCell
+            return cell
+        }
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+extension ProfileVC : UITableViewDelegate{
+}

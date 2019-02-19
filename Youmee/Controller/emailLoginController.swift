557e91c8968bc3799639cc90f2a5707feb9a9f8a
FALLER Romain <romain.faller@gmail.com>
2019-02-20 00:33:00 +0100

    Email login modification #2

--- Youmee/Controller/emailLoginController.swift
+++ Youmee/Controller/emailLoginController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 
-class emailLoginController : UIViewController {
+class emailLoginController : UIViewController, UITextFieldDelegate {
     
     // Variables and UI components
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailLoginButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setBottomBorderToTextFields()
+        emailTextField.delegate = self
+        emailLoginButton.isEnabled=false
+        emailLoginButton.addTarget(self, action: #selector(editingChanged) , for: .editingChanged)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        emailTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func editingChanged(_ sender: UITextField) {
+        if (sender.text == "roro")
+        {
+            emailLoginButton.isEnabled=true
+            emailLoginButton.setBackgroundImage(UIImage(named: "emailLoginButtonActive"), for: .normal)
+        } else
+        {
+            emailLoginButton.isEnabled=false
+            emailLoginButton.setBackgroundImage(UIImage(named: "emailLoginButtonInnactive"), for: .normal)
+        }
+    }
+    
     
     // Method to set the bottom border to the text field
     func setBottomBorderToTextFields()  {
@@ -26,5 +47,11 @@ class emailLoginController : UIViewController {
         bottomLine.frame = CGRect(x: 0, y: emailTextField.frame.height + 5, width: emailTextField.frame.width, height: 1.5)
         bottomLine.backgroundColor = UIColor.darkGray.cgColor
         emailTextField.layer.addSublayer(bottomLine) // Add the bottom line layer
+    }
+    
+    //************ Keyboard beahviours methods
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }

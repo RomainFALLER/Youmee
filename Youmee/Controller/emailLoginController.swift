4964db4a6cd03615f45090bc337bfb7c4001933a
FALLER Romain <romain.faller@gmail.com>
2019-02-19 22:30:25 +0100

    Work on email login page #1.

--- Youmee/Controller/emailLoginController.swift
+++ Youmee/Controller/emailLoginController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class emailLoginController : UIViewController {
     
+    // Variables and UI components
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setBottomBorderToTextFields()
+    }
+    
+    // Method to set the bottom border to the text field
+    func setBottomBorderToTextFields()  {
+        // Create of the bottom line layer
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: emailTextField.frame.height + 5, width: emailTextField.frame.width, height: 1.5)
+        bottomLine.backgroundColor = UIColor.darkGray.cgColor
+        emailTextField.layer.addSublayer(bottomLine) // Add the bottom line layer
     }
 }

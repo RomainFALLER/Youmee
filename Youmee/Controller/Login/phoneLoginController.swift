a28bb33cb5b8f02f0d6251ac59a7708f705213a5
FALLER Romain <romain.faller@gmail.com>
2019-02-22 22:40:44 +0100

    Final work on phone login page.

--- Youmee/Controller/Login/phoneLoginController.swift
+++ Youmee/Controller/Login/phoneLoginController.swift
@@ -10,29 +10,36 @@ import UIKit
 
 class phoneLoginController : UIViewController, UITextFieldDelegate {
     
-    
     @IBOutlet weak var phoneNumberTextField: UITextField!
     @IBOutlet weak var phoneCountryIndicatorTextField: UITextField!
+    @IBOutlet weak var phoneLoginLabel: UILabel!
+    @IBOutlet weak var phoneInformationsLabel: UILabel!
+    @IBOutlet weak var phoneNextButton: UIButton!
+    
     
     @IBAction func backButtonDidTap(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    
     @IBAction func editingChanged(_ sender: Any) {
         if (isValidPhone(testStr: phoneNumberTextField.text ?? "")) {
-            
+            phoneNextButton.setBackgroundImage(UIImage(named: "phoneNextButtonActive") , for: .normal)
         } else {
-            
+            phoneNextButton.setBackgroundImage(UIImage(named: "phoneNextButtonInnactive") , for: .normal)
         }
+    }
     
+    @IBAction func phoneNextButtonDidTap(_ sender: Any) {
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        phoneLoginLabel.font = UIFont.fontWithStyle(ofSize: 17, font: .AvenirNext, style: .DemiBold)
+        phoneInformationsLabel.font = UIFont.fontWithStyle(ofSize: 11, font: .AvenirNext, style: .Regular)
         self.setBottomBorderToTextFields(myTextField: phoneNumberTextField)
         self.setBottomBorderToTextFields(myTextField: phoneCountryIndicatorTextField)
         phoneNumberTextField.delegate = self
+        phoneNextButton.addTarget(self, action: #selector(editingChanged) , for: .editingChanged)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -41,7 +48,7 @@ class phoneLoginController : UIViewController, UITextFieldDelegate {
     
     // Method to validate if the string enterred is an email format
     func isValidPhone(testStr:String) -> Bool {
-        let phoneRegEx = "^\\d{3}-\\d{3}-\\d{4}$"
+        let phoneRegEx = "^\\d{9}$"
         let phoneTest = NSPredicate(format:"SELF MATCHES %@", phoneRegEx)
         return phoneTest.evaluate(with: testStr)
     }
